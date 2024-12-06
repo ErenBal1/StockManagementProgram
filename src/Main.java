f482e62985838a11780d6cc44132ed7535102055
ErenBal <erenbal.academic@gmail.com>
2024-12-06 17:38:34 +0300

    StockManager and Item class added. First version of program.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Burasi eren bal branch'i deneme 123");
-    }
-}
+
+        SwingUtilities.invokeLater(() -> {
+            new StockManager().setVisible(true);
+        });
+}}
